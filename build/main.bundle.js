2786d92316320cad829304ce18f6fe870798a9a2
Narut Promsuparoj <chikado_club2@hotmail.com>
2017-09-01 00:11:33 +0700

    Add index file, script file and css file

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,31 +1,31 @@
-    var isUpper = false;
-    var isShift = false;
-function key(input){
-    let output = document.getElementById("write").textContent;
+"use strict";
+
+var isUpper = false;
+var isShift = false;
+function key(input) {
+    var output = document.getElementById("write").textContent;
     if (isUpper) {
         output = output + String(input).toLocaleUpperCase();
         document.getElementById("write").innerHTML = output;
-    }
-    else if (isShift) {
+    } else if (isShift) {
         output = output + String(input).toLocaleUpperCase();
         document.getElementById("write").innerHTML = output;
         isShift = false;
-    }
-    else {
+    } else {
         output = output + input;
         document.getElementById("write").innerHTML = output;
     }
 }
-function del(){
-    let output = document.getElementById("write").textContent;
+function del() {
+    var output = document.getElementById("write").textContent;
     output = output.substring(0, output.length - 1);
     document.getElementById("write").innerHTML = output;
 }
-function upperCase(){
+function upperCase() {
     isUpper = true;
     return isUpper;
 }
-function shift(){
+function shift() {
     isShift = true;
     return isShift;
 }
